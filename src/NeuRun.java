bb846cff7cb91e1771caaffdb8d4146d0d2ce597
HOWEDOIN <lambinated@gmail.com>
2019-05-19 19:00:40 +0800

    commitryout2

--- src/NeuRun.java
+++ src/NeuRun.java
@@ -3,5 +3,6 @@ import java.util.*;
 public class NeuRun {
     public static void main(String[] args) {
         System.out.println("trying out intellij");
+        // here's a change of code let's see if you detect it 
     }
 }
